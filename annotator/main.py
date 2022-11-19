12a7553f701795292b9674a287382a2c0f5eb209
Joyal Mathew <joyal.mathew2023@gmail.com>
2022-11-18 23:21:32 -0500

    dummy dataset

--- annotator/main.py
+++ annotator/main.py
@@ -1,23 +1,31 @@
 import itertools
 import cv2
+import time
+import numpy as np
 
-video = cv2.VideoCapture("sample-5s.mp4")
+video = cv2.VideoCapture("data/sample-5s.mp4")
 
 if not video.isOpened():
     print("[ERR]\tCould not open video")
     exit(1)
 
+fps = video.get(cv2.CAP_PROP_FPS)
+mspf = int(1000 / fps)
 windowName = "Annotator"
 cv2.namedWindow(windowName)
 
-for i in itertools.count():
-    print(f"Frame: {i}")
+delta = 0
 
+for i in itertools.count():
     result, frame = video.read()
 
     if not result:
-        print("Could not read frame")
-        cv2.destroyWindow(windowName)
         break
 
-    cv2.imshow(windowName, frame)
+    cv2.imwrite(f"dataset/input/img{i:03}.png", frame)
+
+cv2.destroyWindow(windowName)
+zeros = np.zeros(2)
+print(zeros.shape)
+
+np.save("dataset/output.npy", zeros)
